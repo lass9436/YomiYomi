6d63990c2e019a0f768a5e928aee9482e7dea3d7
lass9436 <lass9436@gmail.com>
2025-05-31 02:40:30 +0900

    fix: DummyKanjiQuizViewModel 수정

--- app/src/main/java/com/lass/yomiyomi/viewmodel/kanjiQuiz/DummyKanjiQuizViewModel.kt
+++ app/src/main/java/com/lass/yomiyomi/viewmodel/kanjiQuiz/DummyKanjiQuizViewModel.kt
@@ -2,6 +2,7 @@ package com.lass.yomiyomi.viewmodel.kanjiQuiz
 
 import com.lass.yomiyomi.data.model.Level
 import com.lass.yomiyomi.domain.model.KanjiQuiz
+import com.lass.yomiyomi.domain.model.KanjiQuizType
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -15,23 +16,32 @@ class DummyKanjiQuizViewModel : KanjiQuizViewModelInterface {
     private val _isLoading = MutableStateFlow(false)
     override val isLoading: StateFlow<Boolean> get() = _isLoading
 
-    override fun loadQuizByLevel(level: Level) {
+    override fun loadQuizByLevel(level: Level, quizType: KanjiQuizType, isLearningMode: Boolean) {
         // 더미 데이터를 즉시 반환
         _isLoading.value = true
 
         // 퀴즈 데이터를 구성
-        val options = listOf("ひ / 불", "か / 화재", "ほのお / 화염", "ひかり / 빛") // 수정
-        val correctIndex = 0 // 첫 번째 옵션 "ひ / 불"이 정답
-        val dummyKanji = "火" // Kanji 문자
+        val question = if (quizType == KanjiQuizType.KANJI_TO_READING_MEANING) "火" else "ひ / 불"
+        val answer = if (quizType == KanjiQuizType.KANJI_TO_READING_MEANING) "ひ / 불" else "火"
+        val options = if (quizType == KanjiQuizType.KANJI_TO_READING_MEANING) {
+            listOf("ひ / 불", "か / 화재", "ほのお / 화염", "ひかり / 빛")
+        } else {
+            listOf("火", "炎", "熱", "光")
+        }
+        val correctIndex = 0
 
         // KanjiQuiz 객체 생성
         _quizState.value = KanjiQuiz(
-            kanji = dummyKanji,
-            correctString = options[correctIndex],
-            optionStrings = options,
+            question = question,
+            answer = answer,
+            options = options,
             correctIndex = correctIndex
         )
 
         _isLoading.value = false
+    }
+
+    override fun checkAnswer(selectedIndex: Int, isLearningMode: Boolean) {
+        // 더미 구현에서는 아무 동작도 하지 않음
     }
 }
