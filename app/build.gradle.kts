fa451a601da385be74c47c9233bf041d9fd8bd9b
lass9436 <lass9436@gmail.com>
2025-06-02 06:21:10 +0900

    build: 앱 버전 코드 업데이트
    - 버전 코드를 4에서 5로 변경
    - 최신 빌드에 따른 버전 관리 반영

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.lass.yomiyomi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
