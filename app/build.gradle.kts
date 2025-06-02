7c5ae8102fb2aba5527ba67fac6365a57213f26b
lass9436 <lass9436@gmail.com>
2025-06-03 04:21:02 +0900

    chore: 앱 버전 코드 업데이트
    - versionCode를 5에서 6으로 업데이트
    - 새 버전 배포를 위한 버전 코드 증가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.lass.yomiyomi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
