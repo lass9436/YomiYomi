6a2b10843f8cfe18b98f18c6eba822cf3f30ab07
lass9436 <lass9436@gmail.com>
2025-06-12 14:01:36 +0900

    build: 앱 버전 코드 업데이트
    - versionCode를 10에서 11로 변경
    - 최신 빌드 및 릴리스 준비

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.lass.yomiyomi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
